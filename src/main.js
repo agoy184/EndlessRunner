d6110b9bdb59b4d1b31c55d5ecf20ab86e995bec
Abel Goy <agoy@ucsc.edu>
2023-05-14 16:14:04 -0700

    Creative tilt justification added

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 // Abel Goy 
 // Granny Runner 
-// * hours spent on project 
-// Creative tilt justification:
+// 23 hours spent on project 
+// Creative tilt justification: 
+//  In terms of technicality, I'm particularly proud of the pushback mechanic I created with the fire hydrant obstacle in conjunction with the random obstacle spawning.
+//  I made it so that the game would result in a game over if the player was caught in between the left side of the screen and the hydrant, which pushes the player back.
+//  Combining this with the fact that the car/hydrant spawn positions are randomly generated, this creates random 'traps' for players who spend too much time on the sidewalk, 
+//  since they may be caught in between the car and the hydrant, discouraging camping in the sidewalk lanes. 
+//
+//  In terms of visual style, I'm particularly proud of the sprites that I created for the game.  I don't have that much experience creating art assets in general,
+//  but making all of the sprite assets by myself for this game was fun and I like how the sprites came out.  I also thought the sound effects in conjunction with the music I chose
+//  were funny when considering the feel of the game.  Upbeat music interrupted by a car crash was a funny combination. 
 
 let config = {
     type: Phaser.AUTO,
